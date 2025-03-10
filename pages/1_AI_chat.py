32d726afc1c46edca9525a6e99d9354adae419a7
01CreateWorld <83858349+01CreateWorld@users.noreply.github.com>
2025-03-10 11:55:27 +0800

    Update 1_AI_chat.py

--- pages/1_AI_chat.py
+++ pages/1_AI_chat.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 #st.title("AI Chat")
 
 #st.write("Welcome to the AI Chat page")
